8cf4e4fb0fd841dc0f2aa5cea3fb64ac2e37be25
yildirimGulay <gulaycontact@gmail.com>
2021-06-22 21:47:38 +0100

    edit expense-reducer

--- src/reducers/expenseReducer.js
+++ src/reducers/expenseReducer.js
@@ -1,13 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+
+
+
 export const expenseReducer=(state,action)=>{
 
 switch(action.type){
     case 'ADD_ITEMS':
         return[...state,{
-            title: action.expenses.title,
-            amount: action.expenses.amount,
-            id:action.expenses.id
+            title: action.expense.title,
+            amount: action.expense.amount,
+            id:uuidv4()
         }]
 
+    case 'DELETE_ITEMS':
+        return state.filter((i) => i.id !== action.id);
+        
         default:
             return state
 
